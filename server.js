e486a0304b5cbda6100555bf5070526ccc557586
RikoGit <r.git@lenta.ru>
2020-01-06 02:38:12 +0300

    Switch to ESM

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-'use strict';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 
-const express = require('express');
-const {join} = require('path');
+import express from 'express';
 
+const rootDir = dirname(fileURLToPath(import.meta.url));
 const PORT = process.env.PORT || 5000;
 const server = express();
 
-server.set(`port`, PORT);
+server.set('port', PORT);
 
-server.use(`/`, express.static(join(__dirname, 'static')));
+server.use('/', express.static(join(rootDir, 'static')));
 
 server.listen(PORT, () => {
-    console.log(`Start server on port ${PORT}.`);
+  /* eslint-disable-next-line no-console */
+  console.log(`Start server on port ${PORT}.`);
 });
