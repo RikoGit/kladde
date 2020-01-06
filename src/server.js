e99dd00994b381ed5642d9e5d1008b889b75d010
RikoGit <r.git@lenta.ru>
2020-01-06 03:47:15 +0300

    Fix eslint parser

--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
-import { dirname, join } from 'path';
+import { join } from 'path';
 import { fileURLToPath } from 'url';
 
 import express from 'express';
 
-const rootDir = dirname(fileURLToPath(import.meta.url));
+const rootDir = join(fileURLToPath(import.meta.url), '..', '..');
+
+const staticDir = join(rootDir, 'static');
 const PORT = process.env.PORT || 5000;
 const server = express();
 
 server.set('port', PORT);
 
-server.use('/', express.static(join(rootDir, 'static')));
+server.use('/', express.static(staticDir));
 
 server.listen(PORT, () => {
   /* eslint-disable-next-line no-console */
