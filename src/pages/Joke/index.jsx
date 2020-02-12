63279c6a6e81123b220ef8963a4063e8b6e1e9b6
RikoGit <r.git@lenta.ru>
2020-02-12 03:29:09 +0300

    Work with form

--- src/pages/Joke/index.jsx
+++ src/pages/Joke/index.jsx
@@ -1,11 +1,36 @@
 import React, { useState } from 'react';
 
 const Joke = () => {
+    const [form, setForm] = useState({
+        firstName: '',
+        lastName: '',
+        age: '',
+        gender: '',
+        destination: '',
+        isOverweight: false,
+        isAlcohol: false,
+        isSmoking: false,
+    });
+
+    const handleChange = event => {
+        const { name, value, type, checked } = event.target;
+
+        if (type === 'checkbox') {
+            setForm({ ...form, [name]: checked });
+        } else setForm({ ...form, [name]: value });
+    };
+
+    /*
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [age, setAge] = useState('');
     const [gender, setGender] = useState('');
     const [destination, setDestination] = useState('');
+    const [restrictions, setRestrictions] = useState({
+        isOverweight: false,
+        isAlcohol: false,
+        isSmoking: false,
+    });
 
     const f = {
         firstName: setFirstName,
@@ -13,6 +38,7 @@ const Joke = () => {
         age: setAge,
         gender: setGender,
         destination: setDestination,
+        restrictions: setRestrictions,
     };
 
     const test = (a, b) => {
@@ -20,11 +46,20 @@ const Joke = () => {
     };
 
     const handleChange = event => {
-        const { name, value } = event.target;
+        const { name, value, type, checked } = event.target;
 
         test(name, value);
-        // setFirstName(value);
     };
+    */
+    /*
+    const [form, setForm] = useState({ firstName: '', lastName: '' });
+
+    const handleChange = event => {
+        const { name, value } = event.target;
+
+        setForm({ ...form, [name]: value });
+    };
+    */
 
     return (
         <main>
@@ -33,17 +68,17 @@ const Joke = () => {
                     name="firstName"
                     placeholder="First Name"
                     onChange={handleChange}
-                    value={firstName}
+                    value={form.firstName}
                 />
                 <br />
                 <input
                     name="lastName"
                     placeholder="Last Name"
                     onChange={handleChange}
-                    value={lastName}
+                    value={form.lastName}
                 />
                 <br />
-                <input name="age" placeholder="Age" onChange={handleChange} value={age} />
+                <input name="age" placeholder="Age" onChange={handleChange} value={form.age} />
                 <br />
                 <label>
                     <input
@@ -51,7 +86,7 @@ const Joke = () => {
                         type="radio"
                         value="male"
                         onChange={handleChange}
-                        checked={gender === 'male'}
+                        checked={form.gender === 'male'}
                     />
                     male
                 </label>
@@ -61,16 +96,45 @@ const Joke = () => {
                         type="radio"
                         value="female"
                         onChange={handleChange}
-                        checked={gender === 'female'}
+                        checked={form.gender === 'female'}
                     />
                     female
                 </label>
                 <br />
-
-                {/* Здесь создайте поле со списком для выбора пункта назначения */}
+                <select name="destination" onChange={handleChange} value={form.destination}>
+                    <option value="">-- Please Choose a destination --</option>
+                    <option value="town1">Town1</option>
+                    <option value="town2">Town2</option>
+                    <option value="town3">Town3</option>
+                </select>
                 <br />
-
-                {/* Здесь создайте флажки для указания диетологических ограничений */}
+                <label>
+                    <input
+                        name="isOverweight"
+                        type="checkbox"
+                        checked={form.isOverweight}
+                        onChange={handleChange}
+                    />
+                    overweight
+                </label>
+                <label>
+                    <input
+                        name="isAlcohol"
+                        type="checkbox"
+                        checked={form.isAlcohol}
+                        onChange={handleChange}
+                    />
+                    alcohol
+                </label>
+                <label>
+                    <input
+                        name="isSmoking"
+                        type="checkbox"
+                        checked={form.isSmoking}
+                        onChange={handleChange}
+                    />
+                    smoking
+                </label>
                 <br />
 
                 <button type="submit">Submit</button>
@@ -79,15 +143,19 @@ const Joke = () => {
             <h2>
                 <font color="#3AC1EF">Entered information:</font>
             </h2>
+            {form.firstName || form.lastName ? (
+                <p>
+                    Your name: {form.firstName} {form.lastName}
+                </p>
+            ) : null}
+            {form.age ? <p>Your age: {form.age}</p> : null}
+            {form.gender ? <p>Your gender: {form.gender}</p> : null}
+            {form.destination ? <p>Your destination: {form.destination}</p> : null}
             <p>
-                Your name: {firstName} {lastName}
-            </p>
-            <p>Your age: {age}</p>
-            <p>Your gender: {gender}</p>
-            <p>Your destination: {destination}</p>
-            <p>
-                Your dietary restrictions:
-                {/* Список диетологических ограничений */}
+                Your dietary restrictions: <br />
+                overweight: {form.isOverweight ? 'Yes' : 'No'} <br />
+                alcohol: {form.isAlcohol ? 'Yes' : 'No'} <br />
+                smoking: {form.isSmoking ? 'Yes' : 'No'}
             </p>
         </main>
     );
